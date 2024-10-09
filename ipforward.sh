5e702cbff0c7bd29eb34293d0353b82108be8a7d
alozoBack <alozoBack@github.com>
2024-10-09 17:36:19 +0500

    create ipforwarding script

---
+++ ipforward.sh
@@ -0,0 +1,38 @@
+# Script for forward ip with iptables
+# 
+# Author: alozo(github.com/alozoBack)
+
+
+clear
+
+echo ""
+
+
+echo " /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\ ";
+echo "( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )";
+echo " > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ < ";
+echo " /\_/\                                                                                                    /\_/\ ";
+echo "( o.o )      ._____________  __________________ __________ __      __  _____ __________________          ( o.o )";
+echo " > ^ <       |   \______   \ \_   _____\_____  \\______   /  \    /  \/  _  \\______   \______ \          > ^ < ";
+echo " /\_/\       |   ||     ___/  |    __)  /   |   \|       _\   \/\/   /  /_\  \|       _/|    |  \         /\_/\ ";
+echo "( o.o )      |   ||    |      |     \  /    |    |    |   \\        /    |    |    |   \|    `   \       ( o.o )";
+echo " > ^ <       |___||____|      \___  /  \_______  |____|_  / \__/\  /\____|__  |____|_  /_______  /        > ^ < ";
+echo " /\_/\                            \/           \/       \/       \/         \/       \/        \/         /\_/\ ";
+echo "( o.o )                                                                                                  ( o.o )";
+echo " > ^ <                                                                                                    > ^ < ";
+echo " /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\ ";
+echo "( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )";
+echo " > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ < ";
+
+echo ""
+
+echo "Origin IP: "
+read origin
+echo ""
+echo "Destination IP: "
+read dest
+echo ""
+echo "Port: "
+read port
+sudo iptables -t nat -A PREROUTING -p tcp -d $origin --dport $port -j DNAT --to-destination $dest
+sudo iptables -A FORWARD -p tcp -d $dest --dport $port -j ACCEPT
