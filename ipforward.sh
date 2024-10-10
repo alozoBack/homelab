aba4945f51b3d67d9ea57cb609ed09956c07442a
alozo <108485035+alozoBack@users.noreply.github.com>
2024-10-10 17:27:59 +0500

    Rewrite script to use nft
    no cats :( very sad

--- ipforward.sh
+++ ipforward.sh
@@ -1,49 +1,76 @@
-# Script for forward ip with iptables
-# 
-# Author: alozo(github.com/alozoBack)
+#!/bin/bash
+# Script for forwarding IP with nftables
+#
+# Author: alozo (github.com/alozoBack)
 
 
-clear
+# Usage info
+function show_help {
+    echo "Usage: $0 [-t] [-u] [-d destination] [-o origin] [-p port]"
+    echo "  -t                 Use TCP protocol"
+    echo "  -u                 Use UDP protocol"
+    echo "  -d destination     Destination IP"
+    echo "  -o origin          Origin IP"
+    echo "  -p port            Port to forward"
+}
 
-echo ""
+# Initialize variables
+protocol=""
+origin=""
+dest=""
+port=""
 
+# Parse command line arguments using getopts
+while getopts "tud:o:p:h" opt; do
+  case ${opt} in
+    t )
+      protocol="tcp"
+      ;;
+    u )
+      protocol="udp"
+      ;;
+    d )
+      dest=$OPTARG
+      ;;
+    o )
+      origin=$OPTARG
+      ;;
+    p )
+      port=$OPTARG
+      ;;
+    h )
+      show_help
+      exit 0
+      ;;
+    \? )
+      show_help
+      exit 1
+      ;;
+  esac
+done
 
-echo " /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\ ";
-echo "( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )";
-echo " > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ < ";
-echo " /\_/\                                                                                                    /\_/\ ";
-echo "( o.o )      ._____________  __________________ __________ __      __  _____ __________________          ( o.o )";
-echo " > ^ <       |   \______   \ \_   _____\_____  \\______   /  \    /  \/  _  \\______   \______ \          > ^ < ";
-echo " /\_/\       |   ||     ___/  |    __)  /   |   \|       _\   \/\/   /  /_\  \|       _/|    |  \         /\_/\ ";
-echo "( o.o )      |   ||    |      |     \  /    |    |    |   \\        /    |    |    |   \|    |   \       ( o.o )";
-echo " > ^ <       |___||____|      \___  /  \_______  |____|_  / \__/\  /\____|__  |____|_  /_______  /        > ^ < ";
-echo " /\_/\                            \/           \/       \/       \/         \/       \/        \/         /\_/\ ";
-echo "( o.o )                                                                                                  ( o.o )";
-echo " > ^ <                                                                                                    > ^ < ";
-echo " /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\  /\_/\ ";
-echo "( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )( o.o )";
-echo " > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ <  > ^ < ";
+# Check if all required arguments are provided
+if [ -z "$protocol" ] || [ -z "$origin" ] || [ -z "$dest" ] || [ -z "$port" ]; then
+  echo "Error: Missing required arguments."
+  show_help
+  exit 1
+fi
 
-echo ""
+# Make sure nftables table and chains exist
+nft add table ip nat
+nft add chain ip nat prerouting { type nat hook prerouting priority 0 \; }
+nft add chain ip nat postrouting { type nat hook postrouting priority 100 \; }
 
-echo "Origin IP: "
-read origin
-echo "Destination IP: "
-read dest
-echo "Port: "
-read port
-echo "Protocol(udp/tcp): "
-read protocol
-case $protocol in
-    "tcp")
-    sudo iptables -t nat -A PREROUTING -p tcp -d $origin --dport $port -j DNAT --to-destination $dest:$port ; \
-    sudo iptables -A FORWARD -p tcp -d $dest --dport $port -j ACCEPT
-        ;;
-    "udp")
-    sudo iptables -t nat -A PREROUTING -p udp -d $origin --dport $port -j DNAT --to-destination $dest:$port ; \
-    sudo iptables -A FORWARD -p udp -d $dest --dport $port -j ACCEPT
-        ;;
-        *)
-    echo "invalid"
-    ;;
-esac
+# Apply nftables rules based on protocol
+if [ "$protocol" == "tcp" ]; then
+  nft add rule ip nat prerouting ip daddr $origin tcp dport $port dnat to $dest:$port
+  nft add rule ip nat postrouting ip saddr $dest oifname eth0 masquerade
+elif [ "$protocol" == "udp" ]; then
+  nft add rule ip nat prerouting ip daddr $origin udp dport $port dnat to $dest:$port
+  nft add rule ip nat postrouting ip saddr $dest oifname eth0 masquerade
+else
+  echo "Invalid protocol selected."
+  exit 1
+fi
+
+echo "Forwarding rule added: $origin -> $dest on port $port using $protocol"
